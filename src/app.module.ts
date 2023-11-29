c064170e1d440b51768087c6d1bbba4e1424fce6
jinu0729 <jinu0729@gmail.com>
2023-11-29 17:12:17 +0900

    Feat: TypeOrmConfig 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,29 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { RefreshToken } from './auth/entities/refresh-token.entity';
 import { PagesModule } from './pages/pages.module';
 import { AwsModule } from './aws/aws.module';
+import { typeOrmConfig } from './configs/type-orm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User, RefreshToken],
-      synchronize: true,
-      logging: true,
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     AuthModule,
     PagesModule,
