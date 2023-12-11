240bd16f3a03695e124bf2b0e7468ee93e663de1
jinu0729 <jinu0729@gmail.com>
2023-12-11 11:44:20 +0900

    Feat: logout 기능 useGuards 추가

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { UsersResponseDto } from '../../users/controllers/dtos/responses/users-response.dto';
 import { AuthRequestDto } from './dtos/requests/auth-request.dto';
@@ -6,6 +6,7 @@ import { ResponseEntity } from '../../configs/response-entity';
 import { ApiOperation } from '@nestjs/swagger';
 import { GetUser } from '../decorator/get-user.decorator';
 import { User } from '../../users/entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/auth')
 export class AuthController {
@@ -21,6 +22,7 @@ export class AuthController {
   }
 
   @Delete('/logout')
+  @UseGuards(AuthGuard())
   @ApiOperation({ summary: '로그아웃 API', description: '로그아웃 API' })
   async logout(@GetUser() user: User): Promise<ResponseEntity<string>> {
     return this.authService.logout(user);
