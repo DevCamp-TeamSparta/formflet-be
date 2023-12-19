1932f72ddff4eab15040489be8936737a1b0bb7c
jinu0729 <jinu0729@gmail.com>
2023-12-19 10:59:24 +0900

    Fix: pagesContentRepo 수정

--- src/pages/repositories/pages-content.repository.ts
+++ src/pages/repositories/pages-content.repository.ts
@@ -1,6 +1,7 @@
 import { Repository } from 'typeorm';
 import { PageDetail } from '../entities/page-detail.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Page } from '../entities/page.entity';
 
 export class PagesContentRepository extends Repository<PageDetail> {
   constructor(
@@ -8,5 +9,9 @@ export class PagesContentRepository extends Repository<PageDetail> {
     private readonly repository: Repository<PageDetail>,
   ) {
     super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  async findByPage(page: Page): Promise<PageDetail> {
+    return await this.repository.findOneBy({ page: { id: page.id } });
   }
 }
