14922d66bc71f6a4283df2a6db49d901c3597b1f
jinu0729 <jinu0729@gmail.com>
2023-12-20 10:29:25 +0900

    Feat: formsResponseDto 생성

--- src/forms/controllers/dtos/responses/forms-response.dto.ts
+++ src/forms/controllers/dtos/responses/forms-response.dto.ts
@@ -1,7 +1,17 @@
-export class FormsResponseDto {
-  readonly id: number;
+import { FormDetail } from '../../../entities/forms-detail.entity';
 
-  constructor(id: number) {
+export class FormsResponseDto {
+  id: number;
+  status: boolean;
+  replyStatus: boolean;
+  guide: string;
+  formDetail: FormDetail[];
+
+  constructor(id: number, status: boolean, replyStatus: boolean, guide: string, formDetail: FormDetail[]) {
     this.id = id;
+    this.status = status;
+    this.replyStatus = replyStatus;
+    this.guide = guide;
+    this.formDetail = formDetail;
   }
 }
