48faba182ec20897f8787b7276122bb05b73826a
jinu0729 <jinu0729@gmail.com>
2024-01-05 15:55:51 +0900

    Fix: authGuard TypeError 수정

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -1,4 +1,10 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import * as jwt from 'jsonwebtoken';
 
@@ -7,16 +13,24 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
 
-    const token = request.headers.authorization.replace('Bearer ', '');
-
     try {
+      const token = request.headers.authorization.replace('Bearer ', '');
+
       const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+
       request.user = decodedToken;
-    } catch (error) {
-      if (error.name === 'TokenExpiredError') {
+    } catch (e) {
+      console.log(e);
+
+      if (e.name === 'TokenExpiredError') {
         throw new UnauthorizedException('Expired Token');
+      } else if (e.name === 'JsonWebTokenError') {
+        throw new UnauthorizedException('Invalid Signature');
+      } else if (e.name === 'TypeError') {
+        throw new NotFoundException('Not Found AccessToken');
       }
-      throw new UnauthorizedException('Unauthorized');
+
+      throw new InternalServerErrorException();
     }
 
     return super.canActivate(context);
